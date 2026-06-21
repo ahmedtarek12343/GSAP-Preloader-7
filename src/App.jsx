8845c670f6d5394f8385437f8a01f9c3fa9877fc
ahmedtarek12343 <131623405+ahmedtarek12343@users.noreply.github.com>
2026-06-21 23:32:43 +0300

    added counter

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import { useEffect, useRef } from "react";
 import gsap from "gsap";
 import CustomEase from "gsap/src/CustomEase";
 import { SplitText } from "gsap/src/all";
-gsap.registerPlugin(CustomEase, SplitText);
+import TextPlugin from "gsap/TextPlugin";
+gsap.registerPlugin(CustomEase, SplitText, TextPlugin);
 CustomEase.create("hop", "0.9,0,0.1,1");
 CustomEase.create("glide", "0.8,0,0.2,1");
 function App() {
@@ -22,19 +23,38 @@ function App() {
       type: "chars",
       mask: "chars",
     });
+
     gsap
       .timeline()
+      .from(".number", {
+        y: 80,
+        duration: 1,
+      })
+      .to(".number", { text: "10", duration: 0.6 })
+      .to(".number", { text: "23", duration: 0.6 })
+      .to(".number", { text: "39", duration: 0.6 })
+      .to(".number", { text: "59", duration: 0.6 })
+      .to(".number", { text: "66", duration: 0.6 })
+      .to(".number", { text: "81", duration: 0.3 })
+      .to(".number", { text: "99", duration: 0.3 })
+      .set(".number", {
+        text: "100",
+      })
+      .to(".number", {
+        y: -80,
+        duration: 1,
+      })
       .from(".main", {
         clipPath: "inset( 100% 0  0%  0)",
         ease: "power1.inOut",
-        duration: 1,
+        duration: 0.64,
       })
       .from(
         ".preloader",
         {
           clipPath: "inset(100% 0 0% 0)",
           ease: "power1.inOut",
-          duration: 1,
+          duration: 0.64,
         },
         "<",
       )
@@ -43,16 +63,16 @@ function App() {
         {
           scale: 1,
           ease: "power1.inOut",
-          duration: 1,
+          duration: 0.64,
         },
-        "<0.9",
+        "<0.57",
       )
       .to(
         ".preloader",
         {
           clipPath: "inset(0% 0 100% 0)",
           ease: "power1.out",
-          duration: 1,
+          duration: 0.64,
         },
         "<",
       )
@@ -101,6 +121,11 @@ function App() {
         </div>
         <div className="preloader fixed top-0 left-0 inset-0 bg-black"></div>
       </div>{" "}
+      <div className="fixed top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 text-xl">
+        <div class="overflow-hidden">
+          <div className="number">0</div>
+        </div>
+      </div>
     </>
   );
 }
